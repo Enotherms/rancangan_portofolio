fd5786fb5954ff3f07bf4044b76741df86c0a9fa
Enotherms <sayabiyyu@gmail.com>
2023-04-22 17:42:11 +0700

    modified:   public/css/style.css 	new file:   public/img/IMG_6946.png 	new file:   public/img/logo/linkedin.png 	new file:   public/img/logo/twitter.png 	modified:   public/index.html 	modified:   src/tailwind.css 	modified:   tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./public/**/*.{html,js}"],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+    },
   },
   plugins: [],
 }
